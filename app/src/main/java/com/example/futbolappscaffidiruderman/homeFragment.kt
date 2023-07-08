c47b628b15646b4d20db0d7fbb94528a07cdae5e
aguscaffidi <aguscaffidi@gmail.com>
2023-07-07 23:06:32 -0300

    update

--- app/src/main/java/com/example/futbolappscaffidiruderman/homeFragment.kt
+++ app/src/main/java/com/example/futbolappscaffidiruderman/homeFragment.kt
@@ -7,11 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.futbolappscaffidiruderman.adapter.futbolAdapter
 
 class homeFragment : Fragment() {
-        lateinit var v: View
-        val SharedViewModel : SharedViewModel by activityViewModels()
+       //private lateinit var v: View
+
+    private lateinit var v:View
+    val SharedViewModel : SharedViewModel by activityViewModels()
 
     companion object {
         fun newInstance() = homeFragment()
@@ -22,21 +27,29 @@ class homeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-
+        savedInstanceState: Bundle?,
 
     ): View? {
 
         v = inflater.inflate(R.layout.fragment_home, container, false)
 
-        fun initRRecyclerView(){
-
-        }
-
-
+        initRecyclerView()
 
         return v
     }
+    private fun initRecyclerView(){
 
-  }
+        val recyclerView = v.findViewById<RecyclerView>(R.id.recyclerFutbol)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = futbolAdapter(futbolProvider.clubesList)
+
+
+    }
+
+    private fun LinearLayoutManager(homeFragment: Fragment): LinearLayoutManager {
+        return LinearLayoutManager(this)
+    }
+
+
+}
 
